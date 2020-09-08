dc8854bde4f2bcec53eba84898a6b438657bf187
thenatural86 <philip.smalls86@gmail.com>
2020-09-08 16:34:15 -0400

    tuples

--- 10.2.py
+++ 10.2.py
@@ -2,3 +2,29 @@ name = input("Enter file:")
 if len(name) < 1:
     name = "mbox-short.txt"
 handle = open(name)
+
+tmp = {}
+
+for line in handle:
+    if not line.startswith('From '):
+        continue
+    line = line.rstrip().split()
+    day = line[4]
+    time = line[5]
+    hr = time.split(":")[0]
+    # print("LINE", line)
+    # # print("DAY", day)
+    # print("TIME", time)
+    # print("HR", hr)
+    tmp[hr] = tmp.get(hr, 0) + 1
+# print(tmp)
+
+lst = []
+for k, v in tmp.items():
+    # print(k, v)
+    tup = (k, v)
+    lst.append(tup)
+lst = sorted(lst)
+# print(lst)
+for k, v in lst:
+    print(k, v)
