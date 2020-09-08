24fd9ebab9d64914a0dfd5d201f5c557270c1157
thenatural86 <philip.smalls86@gmail.com>
2020-09-08 14:12:49 -0400

    dictionaries

--- 9.4.py
+++ 9.4.py
@@ -25,32 +25,3 @@ for word, count in counts.items():
         bigword = word
         bigcount = count
 print(bigword, bigcount)
-
-# #############################################
-
-# name = input("Enter file:")
-# if len(name) < 1:
-#     name = "mbox-short.txt"
-# handle = open(name)
-# # init empty {}
-# counts = {}
-# # iterate through file
-# for line in handle:
-#     # uf file does not start with "From", skip
-#     if not line.startswith('From '):
-#         continue
-#     print(line)
-#     # split line into array and take data from index 1 and save in word variable
-#     # words = line.split()
-#     # words = words[1]
-#     # print(words)
-#     # counts[words] = counts.get(words, 0) + 1
-# print(counts)
-
-# bigcount = None
-# bigword = None
-# for word, count in counts.items():
-#     if bigcount is None or count > bigcount:
-#         bigword = word
-#         bigcount = count
-# print(bigword, bigcount/2)
